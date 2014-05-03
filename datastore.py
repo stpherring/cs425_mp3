8df38652269bc6923df04f940d278cfcc065c1d2
Stephen Herring <stpherring@aol.com>
2014-05-03 16:43:55 -0500

    Added functionality for get, insert, delete, and update

--- datastore.py
+++ datastore.py
@@ -9,23 +9,29 @@ class Datastore:
         self.kv_store = dict()
 
 
-    def get(self, key, level):
+    def get(self, key):
         print "getting " + str(key)
+        return self.kv_store[key]
 
-
-    def insert(self, key, value, level):
+    def insert(self, key, value):
         print "inserting (" + str(key) + ", " + str(value) + ")"
+        self.kv_store[key] = value
 
 
-    def update(self, key, value, level):
+    def update(self, key, value):
         print "updating (" + str(key) + ", " + str(value) + ")"
-
+        if key in kv_store
+            self.kv_store[key] = value
 
     def delete(self, key):
         print "deleting " + str(key)
+        del self.kv_store[key]
 
 
     def repair(self, key):
         """ Performs a repair on the key across all replicas containing the key """
         print "repairing " + str(key)
         all_replicas = all_replica_nums(key)
+
+    def show_all():
+        print self.kv_store
